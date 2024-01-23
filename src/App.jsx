2491c2e8ca6cd7b7a2f27a3bc604047b28553c81
Tushar Balakrishnan <tusharbala284@gmail.com>
2024-01-23 16:07:31 +0530

    Browser router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import './App.css'
 import BackgroundDrop from './components/BackgroundDrop/BackgroundDrop'
 import Navbar from './components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div className='app'>
-      <Navbar />
-      <BackgroundDrop />
-    </div>
+    <BrowserRouter>
+      <div className='app'>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                <Navbar />
+                <Outlet />
+              </>
+            }
+          >
+            <Route path='/' element={<BackgroundDrop />} />
+            {/* Add more nested routes as needed */}
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+
   )
 }
 
