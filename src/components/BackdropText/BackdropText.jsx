7e6a61f56e946490d30e7d8f357c0c4d68fe5c16
Tushar Balakrishnan <tusharbala284@gmail.com>
2024-01-23 15:26:17 +0530

    corrected movie row

--- src/components/BackdropText/BackdropText.jsx
+++ src/components/BackdropText/BackdropText.jsx
@@ -16,19 +16,21 @@ function BackdropText({ logo, title, description, movieDetails, movieOrSeries })
                 <div className="title mb-3">
                     {imageLoaded ? <img className="name-logo" src={`${IMAGE_URL}${logo}?api_key=${API_KEY}`} alt={title} onLoad={handleImageLoad} /> : <h1>{title}</h1>}
                 </div>
-                <div className="description py-2">
-                    <h5 className='fs-5'>{description}</h5>
-                </div>
-                {movieDetails && (
-                    <div className="description-buttons">
-                        <button type="button" className="btn btn-light btn-lg">
-                            <FontAwesomeIcon icon={faPlay} className='pe-3' />Play
-                        </button>
-                        <ModalComponent variant="btn btn-dark btn-lg opacity-75" movieDetails={movieDetails} movieOrSeries={movieOrSeries}>
-                            <FontAwesomeIcon icon={faCircleInfo} className='pe-3' />More Info
-                        </ModalComponent>
+                <div className="text-buttons">
+                    <div className="description py-2">
+                        <h5 className='fs-5'>{description}</h5>
                     </div>
-                )}
+                    {movieDetails && (
+                        <div className="description-buttons">
+                            <button type="button" className="btn btn-light btn-lg">
+                                <FontAwesomeIcon icon={faPlay} className='pe-3' />Play
+                            </button>
+                            <ModalComponent variant="btn btn-dark btn-lg opacity-75" movieDetails={movieDetails} movieOrSeries={movieOrSeries}>
+                                <FontAwesomeIcon icon={faCircleInfo} className='pe-3' />More Info
+                            </ModalComponent>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
